3f92ca779b269f865149d345198b8478c8058f8e
Patrice Peterson <patrice.peterson@itz.uni-halle.de>
2014-04-21 21:51:14 +0200

    Fish: Customize prompt

---
+++ base/fish/functions/fish_prompt.fish
@@ -0,0 +1,57 @@
+function fish_prompt --description "Write out the prompt"
+	set -l last_status "$status"
+	# Cache some stuff
+	if not set -q __fish_prompt_hostname
+		set -g __fish_prompt_hostname (hostname)
+	end
+	if not set -q __fish_prompt_normal
+		set -g __fish_prompt_normal (set_color normal)
+	end
+	if not set -q __fish_prompt_error
+		set -g __fish_prompt_error (set_color $fish_color_error)
+	end
+	if not set -q __fish_prompt_virtualenv
+		set -g __fish_prompt_virtualenv (set_color --bold blue)
+	end
+	if not set -q __fish_prompt_repo
+		set -g __fish_prompt_repo (set_color --bold green)
+	end
+
+	set -g __fish_prompt_color "$__fish_prompt_normal"
+	switch $USER
+		case root
+			if not set --q __fish_prompt_cwd
+				if set -q fish_color_cwd_root
+					set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+				else
+					set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+				end
+			end
+			set -g __fish_prompt_symbol '#'
+
+		case '*'
+			if not set -q __fish_prompt_cwd
+				set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+			end
+			# Check if we're in an active virtualenv
+			if test -e "$VIRTUAL_ENV"
+				set -g __fish_prompt_color "$__fish_prompt_virtualenv"
+			end
+			set -g __fish_prompt_symbol '>'
+	end
+
+	if not test $last_status -eq 0
+		set -g __fish_prompt_color "$__fish_prompt_error"
+	end
+
+	echo -s "$USER" @ "$__fish_prompt_hostname" ' ' (prompt_pwd)
+	echo -n -s " $__fish_prompt_color"
+	if set -q RANGER_LEVEL
+		for _ in (seq (math "$RANGER_LEVEL + 1"))
+			echo -n -s "$__fish_prompt_symbol"
+		end
+	else
+		echo -n -s "$__fish_prompt_symbol"
+	end
+	echo -n -s "$__fish_prompt_normal "
+end
---
+++ base/fish/functions/prompt_pwd.fish
@@ -0,0 +1,49 @@
+function prompt_pwd --description "Print the current working directory, shortened to fit the prompt"
+	# Define those here to establish correct scope
+	set -l curdir "$PWD"
+	set -l curdir_shortened "$curdir"
+	set -l resdir ""
+	set -l trailing_delim ''
+	# Loop over dir names
+	while true
+		if test "$curdir" = "$HOME"
+			set curdir_shortened '~'
+		else if set -q shorten_curdir
+				set curdir_shortened (basename "$curdir" | sed 's|\([^.]\).*|\1|')
+		else
+			set curdir_shortened (basename "$curdir")
+		end
+		# Check if we're in an active virtualenv
+		if test -e "$curdir/.venv" -a -e "$VIRTUAL_ENV"
+			set curdir_color "$__fish_prompt_virtualenv"
+		end
+		# Check if we're in a Mercurial repository
+		if test -e "$curdir/.hg"
+			# Set color if it's not already set by virtualenv
+			if not set -q curdir_color
+				set curdir_color "$__fish_prompt_repo"
+			end
+			set resdir "$curdir_color$curdir_shortened$__fish_prompt_normal☿$__fish_prompt_repo"(cat "$curdir/.hg/branch")"$trailing_delim$resdir"
+		# Check if we're in a Git repository
+		else if test -e "$curdir/.git"
+			if not set -q curdir_color
+				set curdir_color "$__fish_prompt_repo"
+			end
+			set resdir "$curdir_color$curdir_shortened$__fish_prompt_normal±$__fish_prompt_repo"(git rev-parse --abbrev-ref HEAD)"$trailing_delim$resdir"
+		# No repos/virtualenvs
+		else
+			set resdir "$__fish_prompt_cwd$curdir_shortened$trailing_delim$resdir"
+		end
+
+		# Stop at / or ~
+		if test $curdir = '/' -o $curdir = "$HOME"
+			break
+		else
+			set curdir (dirname "$curdir")
+			# Add trailing delimiter after first iteration
+			set trailing_delim "$__fish_prompt_normal"'/'
+			set shorten_curdir true
+		end
+	end
+	echo "$resdir"
+end
