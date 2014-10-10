3151fc741e9e2153ec38ab16950bc8bad38a400e
runiq <runiq@archlinux.us>
2014-10-10 20:06:24 +0200

    Fish: Remove virtualfish support

--- common/python/fish/config.fish
+++
@@ -1,5 +0,0 @@
-# Stuff for virtualfish
-set -x VIRTUALFISH_HOME "$XDG_DATA_HOME/virtualenvs"
-source /usr/share/virtualfish/virtual.fish
-source /usr/share/virtualfish/auto_activation.fish
-source /usr/share/virtualfish/global_requirements.fish
